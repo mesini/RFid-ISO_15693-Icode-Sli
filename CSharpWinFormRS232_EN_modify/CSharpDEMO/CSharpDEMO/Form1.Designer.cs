7f4e57f47909ae46e77598f26498a23a6ac317ed
xavini <ngducha762@gmail.com>
2021-06-03 10:16:35 +0900

    #update software name

--- CSharpWinFormRS232_EN_modify/CSharpDEMO/CSharpDEMO/Form1.Designer.cs
+++ CSharpWinFormRS232_EN_modify/CSharpDEMO/CSharpDEMO/Form1.Designer.cs
@@ -193,7 +193,7 @@
             this.Controls.Add(this.txtResult);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "USBReaderDEMO::CSharp";
+            this.Text = "TagReader（I-code Sli）XKC612R";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
